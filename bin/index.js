d54cf15c773bbd19262f3285765394413da36df0
jesintharnold <jesintharnold@gmail.com>
2023-12-31 20:38:16 +0530

    Merge option added and tested 100%

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-
-console.info("Initating...");
 import pdfcmd from "../lib/command.js";
 const gateway=new pdfcmd();
 gateway.parse();
-gateway.entryPoint.action((option)=>{console.log(option);});
