92df38dd8019a08b4581fa070eebc6f77e20b0ab
jesintharnold <jesintharnold@gmail.com>
2023-12-31 19:37:27 +0530

    split function works tested 100%

--- tests/dummy.js
+++ tests/dummy.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
-const fs = require("fs").promises;
-const Path = require("path");
+import { promises as fs } from "fs";
+import { join, basename,parse} from "path";
 
 async function checkDir(path){
     const files=await fs.readdir(path,{withFileTypes:true}).then((files)=>{
         let temp=files.filter((file)=>file.isFile()&&file.name.toLocaleLowerCase().endsWith(".pdf"));
-        temp=temp.map(file=>Path.join(file.path,file.name));
+        temp=temp.map(file=>{
+            const _path_=join(file.path,file.name);
+            const {name}=parse(_path_);
+            return {
+                "path":_path_,
+                "name":name
+            }
+        });
         return temp;
     });
 
@@ -15,11 +22,11 @@ async function checkDir(path){
     };
 };
 
-checkDir("E:/DOWN-BKP");
+// const _A_=await checkDir("E:/DOWN-BKP");
+// console.log(_A_);
 
-
-
-
+const dir=await fs.mkdir("dummy-check",);
+console.log(dir);
 
 
 
